0afb7e8f4413ba0d80cad4ec6f4c91a09ad518ca
belxz777 <belxz888@gmail.com>
2024-09-03 21:11:20 +0400

    changed main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,79 +4,145 @@ import pint from '../../public/landing.png'
 import { AiFillHeart, AiOutlineAlignRight, AiOutlineCompress, AiOutlineIssuesClose } from "react-icons/ai";
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import { useCloudStorage } from '@tma.js/sdk-react';
 import { useInitData,  useMiniApp,useViewport} from '@tma.js/sdk-react';
 import { ClosingBehavior, postEvent } from '@tma.js/sdk';
 import Link from 'next/link';
+import Reroute from '@/components/Reroute';
+import { GoArrowUpRight } from 'react-icons/go';
+import { getTeamDataByName } from '@/server/getAllTeamData';
 
 export default function General() {
-  //pnpm dev
-  const cloudStorage = useCloudStorage();
-  const initData = useInitData()
+
 const [isTeam, setisTeam] = useState(false)
-  const closingBehaviour = new ClosingBehavior(false, postEvent);
-  closingBehaviour.enableConfirmation()
-  const miniApp = useMiniApp();
-  const viewport = useViewport();
-  // const viewport = initViewport();     
+const [team, setteam] = useState<Team | null>(null)
+const [isLoading , setIsLoading] = useState(true)
+  // const closingBehaviour = new ClosingBehavior(false, postEvent);
+  // closingBehaviour.enableConfirmation()
+  // const miniApp = useMiniApp();
+  // const viewport = useViewport();
+  // // const viewport = initViewport();     
+  const [reroute, setreroute] = useState(false)
+  const router = useRouter()
   const [teamName, setteamName] = useState<string | null>(null)
-  const initDataJson = useMemo(() => {
-    if (!initData) {
-      return 'Init data is empty.'; 
-    }   
-    const { authDate, chat, hash, canSendAfter, queryId, receiver, user, startParam } = initData
-    return JSON.stringify({
-      authDate,
-      chat,
-      hash,
-      canSendAfter,
-      queryId,
-      receiver,
-      user,
-      startParam,
-    }, null, ' ');
-  }, [initData]);
+  // const initDataJson = useMemo(() => {
+  //   if (!initData) {
+  //     return 'Init data is empty.'; 
+  //   }   
+
+  //   // const { teamData, fetchData,isError } = useTeamDataByName(teamName)
+  //   // if (isError) {
+  //   //   setisTeam(false)
+  //   //   return
+  //   // }
+  //   // else{
+  //   //   setteam(teamData)
+  //   //   setisTeam(true)
+  //   // }
+  //   const { authDate, chat, hash, canSendAfter, queryId, receiver, user, startParam } = initData
+  //   return JSON.stringify({
+  //     authDate,
+  //     chat,
+  //     hash,
+  //     canSendAfter,
+  //     queryId,
+  //     receiver,
+  //     user,
+  //     startParam,
+  //   }, null, ' ');
+  // }, [initData]);
+
   useEffect(() => {
-      viewport.expand()
-    miniApp.ready()
-    setteamName( localStorage.getItem("team")?.valueOf() || null)
-    if(teamName){
-setisTeam(true)
+    // viewport.expand()
+    // miniApp.ready()
+    const teamName = localStorage.getItem("team")
+    if (!teamName) {
+      setisTeam(false)
+      setIsLoading(false)
+      throw new Error('No team found');
+      return
     }
+    fetchData(teamName)
   }, [])
-  // last version
+  const fetchData = async (name:string) => {
+    try {
+        const data = await getTeamDataByName(name);
 
+        if (data === null) {
+          setIsLoading(false)
+            setteam(null);
+            throw new Error('No team found');
+        } else if ('statusCode' in data) {
+          setIsLoading(false)
+            setisTeam(false);
+         throw new Error('No team found');
+        } else {
+          setIsLoading(false)
+            setteam(data);
+            setisTeam(true);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
   return (
-<Suspense fallback={<div>Loading...</div>}  >
-    <div className="overflow-hidden h-screen">
-      <header>
-        <div className='flex  justify-between '>
+<Suspense fallback={<Reroute text='Загрузка'/>}  >
+{
+      reroute && <Reroute text="Переход  . . ."/>
+    }
+    {
+      isLoading && <Reroute  text="Загрузка  . . ."/>
+    }
 
-           <Link href={"/qrscanner"} prefetch={false}  className='bg-button-base hover:bg-hint-base text-button-base font-bold py-2 px-4  rounded-lg text-xl w-1/3' >          <AiOutlineCompress />
+    <div className="overflow-hidden h-screen">
+      <header className="bg-scin-base text-scin-base shadow  h-1/5">
+        <div className='flex justify-around '>
+
+
+           <Link href={"/qrscanner"} prefetch={false} onClick={()=>{
+          setreroute(true)
+
+        }}  className='bg-button-base hover:bg-hint-base text-button-base font-bold py-2 px-4 rounded-full text-xl w-1/3' >          <AiOutlineCompress />
            QRcode сканер</Link>
+
           {
-            !teamName ?
-            <>
-                 
-            <Link href={"/start"} prefetch={true}  className=" bg-button-base hover:bg-hint-base text-button-base font-bold py-2 px-4 rounded-lg text-xl w-1/3">              <AiOutlineIssuesClose />  Начать </Link>
-           </>
-          :
-          <>
-        <Link href={"/period"} prefetch={true} className=" bg-button-base hover:bg-hint-base text-button-base font-bold  text-lg py-2 px-4l    rounded-lg w-1/3">   
-         <AiOutlineIssuesClose />   Продолжить
-         </Link>
-         </>
+            team?.name ?
+            <Link href={"/period"} prefetch={true} onClick={()=>{
+              setreroute(true)
+            }}  className=" bg-button-base hover:bg-hint-base text-button-base font-bold py-4  px-4 rounded-full text-xl  ">   
+     <GoArrowUpRight /> Продолжить 
+             </Link>
+:
+<Link href={"/start"} prefetch={true} onClick={()=>{
+  setreroute(true)
+
+}}  className=" bg-button-base hover:bg-hint-base text-button-base font-bold  px-4 rounded-full text-xl  mb-2 ">   
+ <AiOutlineIssuesClose />  Начать  
+ </Link>
+
           }      
-            <button className=" bg-button-base hover:bg-hint-base text-button-base font-bold py-2 px-4  rounded-lg text-xl  w-1/3">
-          <AiFillHeart/>
-         <Link href={"/adminPanel"} prefetch={false}>  Админ</Link>
-        </button>
+
         </div>
 
       </header>
-      <main className='flex w-screen h-screen justify-center items-center flex-col'>
-        <Image src={pint} alt='' className=' bg-button-base rounded-full mb-4   select-none '  loading='lazy'/>
-        <h1 className='text-center  text-xl  text-scin-base font-extrabold'>Заводские Игры: Квестбот по городу на велосипедах</h1>
+      <main className='flex w-screen h-4/5 justify-center items-center flex-col'>
+      {team?.name  && <> <h1 className='text-3xl font-bold tracking-tight text-link-base text-center mb-4'>Ваша команда: {team?.name}</h1>
+      <h2 className='text-2xl font-bold tracking-tight text-link-base text-center mb-4'>Решено : {team?.solved?.length || 0} квеста</h2>
+      </>}
+
+        <Image src={pint} alt=''  
+        onDoubleClick={()=>{
+
+        router.push("/adminPanel")
+        }} className=' bg-button-base rounded-full    select-none '  loading='lazy'/>
+          <button className=" bg-button-base hover:bg-hint-base text-button-base font-bold py-2 px-4  rounded-full text-xl  w-1/3">
+          <AiFillHeart/>
+         <Link href={"/map"} prefetch={false}
+         onClick={()=>{
+          setreroute(true)
+
+        }} 
+         >  Помощь  </Link>
+        </button>
       </main>
     </div>
     </Suspense>
