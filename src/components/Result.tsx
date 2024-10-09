26f143a303f0bab43ebbb3d87ca4e457cd0fc615
belxz777 <belxz888@gmail.com>
2024-10-09 16:12:33 +0400

    .

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -91,9 +91,9 @@ addPhoto(true)
   loading &&
   <div className="flex items-center justify-center h-1/4">
   <div className="flex space-x-2">
-    <div className="w-3 h-3 bg-primary rounded-full animate-bounce [animation-delay:-0.3s]"></div>
-    <div className="w-3 h-3 bg-primary rounded-full animate-bounce [animation-delay:-0.15s]"></div>
-    <div className="w-3 h-3 bg-primary rounded-full animate-bounce"></div>
+    <div className="w-3 h-3 bg-primary rounded-full bg-button-base animate-bounce [animation-delay:-0.3s]"></div>
+    <div className="w-3 h-3 bg-primary rounded-full bg-button-base animate-bounce [animation-delay:-0.15s]"></div>
+    <div className="w-3 h-3 bg-primary rounded-full bg-button-base animate-bounce"></div>
   </div>
 </div>
 }
